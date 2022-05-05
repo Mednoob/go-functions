7c7a235803a2a1e221f98307d0b8c13598e8bc78
Ahmad Yaqdhan <ahmadyaqdhan123@gmail.com>
2022-05-06 08:13:03 +0900

    feat: add OctalNumber

---
+++ OctalNumber.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+    "fmt"
+    "math"
+    "strconv"
+)
+
+func main() {
+    // Octal number of 321 = 3 * 8^2 + 2 * 8^1 + 1 * 8^0 = 209
+    fmt.Println(OctalNumber(321))
+}
+
+func OctalNumber(num int) float64 {
+    var octal float64
+	str := strconv.Itoa(num)
+	for i, v := range str {
+		conv, er := strconv.Atoi(string(v))
+		if er != nil {
+			fmt.Println("Error occured while converting ASCII to Int")
+			return 0
+		}
+		octal += float64(conv) * math.Pow(8, float64(len(str)-1-i))
+	}
+
+	return octal
+}
